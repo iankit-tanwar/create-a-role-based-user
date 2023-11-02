00ee3fca88cc7696be9364f34483745ab95104ee
Ankit Tanwar <tankitofficial01@gmail.com>
2023-11-02 17:00:53 +0530

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import { Inter } from 'next/font/google'
 import './globals.css'
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,123 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+import { redirect } from "next/navigation";
+import { Router } from "next/router";
+import { useRouter } from "next/router";
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
+  const [title, setTitle] = useState('Login System');
+  const [payload, setPayload] = useState({
+    email: "",
+    password: "",
+    role: ""
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    
+    // Update the payload when the role is changed
+    setPayload({
+      ...payload,
+      [name]: value
+    });
+  }
+
+  useEffect(() => {
+    // Set email and password values based on the selected role
+    if (payload.role === "Admin") {
+      setPayload({
+        ...payload,
+        email: "admin@gmail.com",
+        password: "adminPassword"
+      });
+    } else if (payload.role === "Reseller") {
+      setPayload({
+        ...payload,
+        email: "reseller@gmail.com",
+        password: "resellerPassword"
+      });
+    } else if (payload.role === "AccountManager") {
+      setPayload({
+        ...payload,
+        email: "accountmanager@gmail.com",
+        password: "accountmanagerPassword"
+      });
+    } else if (payload.role === "EndUser") {
+      setPayload({
+        ...payload,
+        email: "enduser@gmail.com",
+        password: "enduserPassword"
+      });
+    }
+  }, [payload.role]);
+
+  const submitData = () => {
+    console.log(payload); // Log the payload, including the selected role, email, and password
+
+    if (payload.role === "Admin") {
+    window.location.href ="/dashboard/admin";
+    } else if (payload.role === "Reseller") {
+      window.location.href ="/dashboard/reseller";
+    } else if (payload.role === "AccountManager") {
+      window.location.href ="/dashboard/accountmanager";
+    } else if (payload.role === "EndUser") {
+      window.location.href ="/dashboard/enduser";
+    }
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='container d-flex justify-content-center'>
+      <div className="card mt-5 col-6">
+        <div className="card-header text-center">
+          <h3>{title}</h3>
+        </div>
+        <div className="card-body">
+          <form>
+            <div className="mb-3">
+              <label htmlFor="role" className="form-label">Role</label>
+              <select
+                className="form-select"
+                aria-label="Default select example"
+                name="role"
+                value={payload.role}
+                onChange={handleChange}
+              >
+                <option value="" disabled>Select Your role</option>
+                <option value="Admin">Admin</option>
+                <option value="Reseller">Reseller</option>
+                <option value="AccountManager">AccountManager</option>
+                <option value="EndUser">EndUser</option>
+              </select>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                name="email"
+                value={payload.email}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+                name="password"
+                value={payload.password}
+                onChange={handleChange}
+              />
+            </div>
+          </form>
+        </div>
+        <div className="card-footer text-body-secondary text-center">
+          <button type="submit" className="btn btn-primary" onClick={submitData}>Submit</button>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
